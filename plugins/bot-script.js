383e099966008c4ce9b6faf3882b52ee70a5e3ca
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-28 01:54:56 -0500

    Actualizar bot-script.js

--- plugins/bot-script.js
+++ plugins/bot-script.js
@@ -3,7 +3,7 @@ import moment from 'moment-timezone'
 
 var handler = async (m, { conn, usedPrefix }) => {
 
-let res = await fetch('https://api.github.com/repos/AzamiJs/CuriosityBot-MD')
+let res = await fetch('https://api.github.com/repos/WOTCHITO/Nexus--bot-')
 let json = await res.json()
  
 let git = `*乂  B O T  -  S C R I P T*\n\n◦ *Nombre* : ${json.name}\n◦ *Visitantes* : ${json.watchers_count}\n◦ *Peso* : ${(json.size / 1024).toFixed(2)} MB\n◦ *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n◦ *Url* : ${json.html_url}\n\n	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n${azami}`
