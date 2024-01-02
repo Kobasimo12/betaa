e6dff180759ef7d02dbf50027b2fc0f3b3292811
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-01-02 16:18:31 -0500

    Actualizar juegos-pepon.js

--- plugins/juegos-pepon.js
+++ plugins/juegos-pepon.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
  import fetch from 'node-fetch'; 
  const handler = async (m, {command, conn}) => { 
-   if (!db.data.chats[m.chat].modopepon && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝙿𝙴𝙿𝙾𝙽 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modopepon*'; 
+   if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝙿𝙴𝙿𝙾𝙽 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'; 
 
    if (command == 'nsfwloli') { 
      const res = (await axios.get(`https://raw.githubusercontent.com/Jxtxn17/BaileyBot-MD/master/src/JSON/Gordo-PEPON.json`)).data; 
@@ -11,7 +11,7 @@ import axios from 'axios';
   };
  handler.help = ['pepon, gordo']; 
  handler.command = ['pepon, gordo']; 
- handler.tags = ['pepon']; 
+ handler.tags = ['nsfw']; 
  handler.premium = true 
 
  export default handler;
