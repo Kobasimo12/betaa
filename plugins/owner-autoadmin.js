ca898197b282797ebffe98e9e386c897909b728f
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-01-07 23:46:37 -0500

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {  
 if (m.fromMe) return
-if (isAdmin) throw '*[ 🍓 ] HOLA CREADOR USTED YA TIENE ADMIN EN ESTE GRUPO!*'
+if (isAdmin) throw '*[ 🐼 ] ℍ𝕆𝕃𝔸 ℂℝ𝔼𝔸𝔻𝕆ℝ 𝕌𝕊𝕋𝔼𝔻 𝕐𝔸 𝕋𝕀𝔼ℕ𝔼 𝔸𝔻𝕄𝕀ℕ 𝔼ℕ 𝔼𝕊𝕋𝔼 𝔾ℝ𝕌ℙ𝕆!*'
 try {  
 await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 } catch {
