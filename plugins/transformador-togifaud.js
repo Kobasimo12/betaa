e40f43e9b834e773d72de4e5a11b54a6d007e00a
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2024-01-10 21:13:34 +0530

    Update converter-togifaud.js

--- plugins/transformador-togifaud.js
+++ plugins/transformador-togifaud.js
@@ -1,18 +1,19 @@
 var handler = async (m, {conn, usedPrefix, command}) => {
 
-if (!m.quoted) throw `*⚠️ RESPONDA A UN VÍDEO QUE DESEE CONVERTIR A GIF CON AUDIO*` 
+if (!m.quoted) throw `*⚠️ REPLY TO A VIDEO YOU WANT TO CONVERT TO GIF WITH AUDIO*`
 const q = m.quoted || m
 const mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) throw `*⚠️ MÍMICA ${mime} NO SOPORTADA*` 
-m.reply('*🚀 C A R G A N D O*')
+if (!/(mp4)/.test(mime)) throw `*⚠️ MIMICRY ${mime} NOT SUPPORTED*`
+m.reply('*🚀 C H A R G I N G*')
 const media = await q.download()
 conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*🚀🚀*'}, {quoted: m})
 
 }
 handler.help = ['togifaud']
-handler.tags = ['transformador']
+handler.tags = ['transformer']
 handler.command = ['togifaud']
 
 handler.limit = true
  
 export default handler
+
