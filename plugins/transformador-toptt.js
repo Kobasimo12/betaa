9a3cfb7da1b38bb3cfaf9b46af523df39615b490
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2024-01-10 21:16:28 +0530

    Update converter-toptt.js

--- plugins/transformador-toptt.js
+++ plugins/transformador-toptt.js
@@ -4,20 +4,21 @@ var handler = async (m, { conn, usedPrefix, command }) => {
 
 let q = m.quoted ? m.quoted : m
 let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-if (!/video|audio/.test(mime)) throw `*⚠️ RESPONDE AL VIDEO O AUDIO QUE DESEE CONVERTIR A NOTA DE VOZ*`
+if (!/video|audio/.test(mime)) throw `*⚠️ RESPOND TO THE VIDEO OR AUDIO YOU WANT TO CONVERT TO VOICE NOTE*`
 let media = await q.download?.()
-if (!media && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-if (!media && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
+if (!media && !/video/.test(mime)) throw '*⚠️ AN ERROR OCCURRED, TRY AGAIN*'
+if (!media && !/audio/.test(mime)) throw '*⚠️ AN ERROR OCCURRED, TRY AGAIN*'
 let audio = await toPTT(media, 'mp4')
-if (!audio.data && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-if (!audio.data && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
+if (!audio.data && !/audio/.test(mime)) throw '*⚠️ AN ERROR OCCURRED, TRY AGAIN*'
+if (!audio.data && !/video/.test(mime)) throw '*⚠️ AN ERROR OCCURRED, TRY AGAIN*'
 conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, true, { mimetype: 'audio/mp4' })
 
 }
 handler.help = ['tovn']
-handler.tags = ['transformador']
+handler.tags = ['transformer']
 handler.command = /^to(vn|(ptt)?)$/i
 
 handler.limit = true
 
 export default handler
+
