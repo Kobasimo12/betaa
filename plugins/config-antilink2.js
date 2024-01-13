9f351c3df2e181097b6779b2930a86d025cd5514
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2024-01-13 14:14:22 +0530

    Update config-antilink2.js

--- plugins/config-antilink2.js
+++ plugins/config-antilink2.js
@@ -17,11 +17,11 @@ if (m.text.includes(linkThisGroup)) return !0
 if (m.text.includes(linkThisGroup2)) return !0
 if (m.text.includes(linkThisGroup3)) return !0
 }
-await conn.reply(m.chat, `🚩 *¡Enlace detectado!*\n\n*${await this.getName(m.sender)} rompiste las reglas, seras eliminado*`, m, fake, )
-if (!isBotAdmin) return conn.reply(m.chat, `🚩 *No soy admin, no puedo eliminar intrusos*`, m, fake, )
+await conn.reply(m.chat, `${error_emoji} *Link detected!*\n\n*${await this.getName(m.sender)} you broke the rules, you will be eliminated*`, m, fake, )
+if (!isBotAdmin) return conn.reply(m.chat, `${error_emoji} *I'm not an admin, I can't remove intruders*`, m, fake, )
 if (isBotAdmin && bot.restrict) {
-await conn.groupParticipantsUpdate(m.chat, [m.sender],'remove')
-} else if (!bot.restrict) return conn.reply(m.chat, `*¡Esta característica esta desactivada!*`, m, fake, )
+await conn.groupParticipantUpdate(m.chat, [m.sender],'remove')
+} else if (!bot.restrict) return conn.reply(m.chat, `*This feature is disabled!*`, m, fake, )
 }
 return !0
 
